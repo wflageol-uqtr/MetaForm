8b206b0ed19d814a0a9965175f8cabdbb563d88c
William Flageol <wflageol@gmail.com>
2024-06-11 14:18:45 -0400

    Démonstration git.

--- MetaForm/MetaForm/Program.cs
+++ MetaForm/MetaForm/Program.cs
@@ -8,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+// Ligne de commentaire.
 
 var app = builder.Build();
 
