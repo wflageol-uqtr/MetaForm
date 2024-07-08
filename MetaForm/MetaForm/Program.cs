643cca3028dd4df339168daa1fbe57fe5bfa39df
laidia <ahcene.laidi@uqtr.ca>
2024-07-08 18:18:28 -0400

    Branche ahcene

--- MetaForm/MetaForm/Program.cs
+++ MetaForm/MetaForm/Program.cs
@@ -1,6 +1,12 @@
+
 using MetaForm.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Builder;
+using System.IO;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,14 +15,21 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 
+// Ajouter le service HttpClient
+builder.Services.AddHttpClient();
+
+// Ajoutez les services pour le contrôleur et le service de données de formulaire
+builder.Services.AddSingleton<FormDataService>();
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-	app.UseExceptionHandler("/Error");
-	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-	app.UseHsts();
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -25,6 +38,8 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+// Mappez les contrôleurs pour les API
+app.MapControllers();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
