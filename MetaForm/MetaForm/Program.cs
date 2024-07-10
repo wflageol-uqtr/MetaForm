bbad1f0927dd235a62ac0e2f6ff2315695d8e1e3
William Flageol <wflageol@gmail.com>
2024-07-10 09:59:22 -0400

    Réparation de la branche.

--- MetaForm/MetaForm/Program.cs
+++ MetaForm/MetaForm/Program.cs
@@ -8,21 +8,18 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
-
+builder.Services.AddSingleton<ListService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-	app.UseExceptionHandler("/Error");
-	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-	app.UseHsts();
+    app.UseExceptionHandler("/Error");
 }
 
-app.UseHttpsRedirection();
 
 app.UseStaticFiles();
-
+app.UseHttpsRedirection();
 app.UseRouting();
 
 app.MapBlazorHub();
