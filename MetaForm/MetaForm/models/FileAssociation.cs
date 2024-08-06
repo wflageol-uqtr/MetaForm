d438f054129cc61a06b9f01e6f72cc102ccbb404
Amine Zitoune <zitoune@MATINF.uqtr.ca>
2024-08-06 04:00:00 +0100

    Correction des problèmes d'association de fichiers et de listes

---
+++ MetaForm/MetaForm/models/FileAssociation.cs
@@ -0,0 +1,8 @@
+﻿namespace MetaForm.models
+{
+    public class FileAssociation
+    {
+        public string FileName { get; set; } = string.Empty;
+        public int ListId { get; set; }
+    }
+}
---
+++ MetaForm/MetaForm/models/FormData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace MetaForm.models
+{
+    public class FormData
+    {
+        public string ListName { get; set; } // Nom de la liste
+        public string RecordId { get; set; } // ID de l'enregistrement
+        public Dictionary<string, object> formData { get; set; } // Dictionnaire contenant les données du formulaire
+    }
+}
